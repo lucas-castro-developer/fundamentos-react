d4687c659dcfeb3bdb9325df1023b2b59effa08d
Lucas Castro <lucas.sc98@hotmail.com>
2020-10-21 20:41:33 -0300

    Components with property

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import './index.css'
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Primeiro from './components/basicos/Primeiro'
+import Primeiro from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametro";
 
 ReactDOM.render(
   <div>
     <Primeiro></Primeiro>
+    <ComParametro titulo="Situação do Aluno" aluno="Pedro Silva" nota={9.3} />
+    <ComParametro titulo="Situação do Aluno" aluno="Maria Santos" nota={9.9} />
   </div>,
   document.getElementById("root")
 );
