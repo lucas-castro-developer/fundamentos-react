bce02ed6f2544b800fd61bc045e79aa0514974ef
Lucas Castro <lucas.sc98@hotmail.com>
2020-10-22 20:36:17 -0300

    React Fragment

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import ReactDOM from "react-dom";
 
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
+import Fragmento from "./components/basicos/Fragmento"
 
 ReactDOM.render(
   <div>
     <Primeiro></Primeiro>
     <ComParametro titulo="Situação do Aluno" aluno="Pedro Silva" nota={9.3} />
     <ComParametro titulo="Situação do Aluno" aluno="Maria Santos" nota={9.9} />
+    <Fragmento />
   </div>,
   document.getElementById("root")
 );
